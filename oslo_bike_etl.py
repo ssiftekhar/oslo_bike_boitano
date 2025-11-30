16e80cec87180e40c181d0913ec2dc2e780159f2
Osama Iftekhar <68809388+ssiftekhar@users.noreply.github.com>
2025-12-01 00:30:57 +0100

    Fjernet casebeskrivelse fra toppen av python filen

--- oslo_bike_etl.py
+++ oslo_bike_etl.py
@@ -1,36 +1,3 @@
-"""
-Scenario
-En fiktiv kunde som leverer leie av sykler rundt om i Oslo by har laget endepunkter for sine data. 
-De ønsker nå å begynne å ta datadrevne beslutninger for hvor de skal plassere nye stasjoner. 
-I den forstand har de nå etterspurt at du hjelper de med å tilgjengeliggjøre dataene til deres analytikere og stakeholdere. 
-Din oppgave er å levere en POC ETL-flyt for å hjelpe dem å ta data-drevne beslutninger.
-
-Forretningsmålet
-Målet er å lage brukbare dataprodukter som data analytikere kan bruke til å vise til interessenter. 
-Derfor spør de om å få tilgjengeliggjort ren og pen data fra endepunktene de har hørt om, 
-i et vedvarende og spørrbart format (persistent og query-able). 
-De ønsker også en metrikk på hvor mye brukt stasjonene er, hva nå enn det betyr er ikke spesifisert og blir opp til deg å bestemme. 
-Om det er noe annet kult du også ønsker å gjøre så er det fritt fram. Hvor mye eller lite du gjør ut av oppgaven er opp til deg.
-
-Dataene
-Dataen du skal bruke ligger på REST-endepunkter her.
-
-- API Base-URL: 'https://oslo-bike-api-13322556367.europe-west1.run.app'
-- GET-endepunkter:
-- [station_information] : Dette endepunktet inneholder metadata om sykkelstasjonene. 
-Dette inkluderer ID, navn, adresse, posisjon og kapasitet.
-- [station_status] : Dette endepunktet inneholder data om statusen til sykkelstasjonene akkurat nå.
-- [pass_sales] : Dette punktet inneholder data på salg av tilganger, både månedlige, daglige og enkeltpass.
-
-
- Leveranse:
-Vi håper du har anledning til å levere løsningen i en zip fil over mai eller en link til et åpent repo. Løsningen bør inneholde følgende:
-1. Python-filen(e) som svarer på oppgaven.
-2. en requirements.txt som lister nødvendige pakker så vi får kjørt det, om nødvendig.
-3. en README.md som beskriver hvordan man setter opp og kjører koden, samt en veldig kort beskrivelse av løsningen og eventuelle designvalg.
-"""
-
-
 import requests
 import pandas as pd
 from datetime import datetime
@@ -242,4 +209,5 @@ def main():
     print("\n Ferdig med kjøringen! Alle data lagret i SQLite-basen oslo_bike.db.")
 
 if __name__ == "__main__":
+
     main()
